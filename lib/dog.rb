7116c2de7c8159b759dbb403d7db80d3ceabd3f1
ehachi <ehajianto@gmail.com>
2023-02-08 22:14:22 +0300

    Complet

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-
-end
+    attr_accessor :this_dogs_name
+  
+    def name=(new_name)
+      @this_dogs_name = new_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
